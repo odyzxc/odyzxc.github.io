539401af75fac9e99654c5153b868e16478ce3a4
Szymon Dziedzic <szymon.d@curvestone.io>
2021-09-18 17:06:40 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import React from 'react';
-import './App.css';
-import {amen} from "./chunks/amen";
+import React from "react";
+import "./App.css";
+import { amen } from "./chunks/amen";
 
-function splitString (string: string, size: number) {
-    const re = new RegExp('.{1,' + size + '}', 'g');
-    return string.match(re);
+function splitString(string: string, size: number) {
+  const re = new RegExp(".{1," + size + "}", "g");
+  return string.match(re);
 }
 
-function randomIntFromRange(min: number, max: number ) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+function randomIntFromRange(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function App() {
-  const audioHeader = 'data:audio/wav;base64,';
-    const numberOfChunks = randomIntFromRange(320, 2048);
+  const audioHeader = "data:audio/wav;base64,";
+  const numberOfChunks = randomIntFromRange(320, 2048);
 
-    const chunks = splitString(amen, 64000);
-    let audioSrc = `${audioHeader}${chunks![0]}`
-      for(let i = 0; i < numberOfChunks; i++) {
-        const chunkNumber = randomIntFromRange(1, chunks!.length -1)
-        audioSrc += chunks![chunkNumber]
-      }
-
-      console.log({numberOfChunks, lnght: audioSrc.length})
-      return (
-        <div className="App">
-          <header className="App-header">
-            PLAY SOME RANDOM AMEN BREAKS
-           <div > <audio controls autoPlay loop src={audioSrc}/></div>
-    {/*       <div > <audio controls autoPlay loop src={audioSrc}/></div>*/}
+  const chunks = splitString(amen, 64000);
+  let audioSrc = `${audioHeader}${chunks![0]}`;
+  for (let i = 0; i < numberOfChunks; i++) {
+    const chunkNumber = randomIntFromRange(1, chunks!.length - 1);
+    audioSrc += chunks![chunkNumber];
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        PLAY SOME RANDOM AMEN BREAKS
+        <div>
+          <audio controls autoPlay loop src={audioSrc} />
+        </div>
       </header>
     </div>
   );
